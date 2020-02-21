5ea0215f817f8abaac7416e67274b14dc7171525
Eric Olson <eric.rob.olson@gmail.com>
2020-02-21 08:03:39 -0800

    Continued implementing tests for GameInput.

--- src/input/game_input.rs
+++ src/input/game_input.rs
@@ -1,6 +1,9 @@
 use crate::network;
 use network::bitvector::NIBBLE_SIZE;
 
+use crate::utility;
+use utility::pow;
+
 const NULL_FRAME: i32 = -1;
 const BITS_IN_BYTE: usize = 8;
 
@@ -20,13 +23,15 @@ fn get_empty_bits(max_bytes: usize) -> Vec<bool> {
 
     return bits;
 }
+
 impl GameInput {
+    /// Initializes a new GameInput. Will panic if 2^NIBBLE_SIZE <= max_bytes * max_players * BITS_IN_BYTES.
     pub fn new(
         max_bytes: usize,
         max_players: usize,
         byte_size_of_all_input_for_all_players: usize,
     ) -> Self {
-        if 2 ^ NIBBLE_SIZE <= (max_bytes * max_players * BITS_IN_BYTE) {
+        if pow(2, NIBBLE_SIZE) <= (max_bytes * max_players * BITS_IN_BYTE) {
             panic!("ERROR ALLOCATING MEMORY"); //TODO: expand on why this is bad
         }
 
@@ -39,10 +44,7 @@ impl GameInput {
         };
     }
 
-    pub fn is_null(&self) -> bool {
-        return self.frame == NULL_FRAME;
-    }
-
+    /// Initialize a new frame. If the byte_size_of_all_input > max_bytes * max_players, will panic.
     pub fn init(
         &mut self,
         frame: i32,
@@ -64,6 +66,8 @@ impl GameInput {
             }
         }
     }
+
+    /// Initialize a new frame with an offset. If the byte_size_of_all_input > max_bytes, will panic.
     pub fn init_offset(
         &mut self,
         frame: i32,
@@ -87,6 +91,12 @@ impl GameInput {
         }
     }
 
+    /// Check whether the given frame is null or not.
+    pub fn is_null(&self) -> bool {
+        return self.frame == NULL_FRAME;
+    }
+
+    /// Returns the value at the given index
     pub fn value(&self, i: usize) -> bool {
         return self.bits[i];
     }
@@ -140,7 +150,443 @@ mod tests {
 
     // new tests
     #[test]
-    fn game_input_new_initializes_proper_values() {
-        assert_eq!(true, false);
+    fn game_input_new_sets_proper_values() {
+        let input = GameInput::new(3, 4, 5);
+
+        assert_eq!(3, input.max_bytes);
+        assert_eq!(4, input.max_players);
+        assert_eq!(5, input.byte_size_of_all_input_for_all_players);
+        assert_eq!(0, input.frame);
+        assert_eq!(get_empty_bits(input.max_bytes), input.bits);
+    }
+
+    #[test]
+    #[should_panic]
+    fn game_input_new_panics_with_bad_values() {
+        let input = GameInput::new(99, 9, 5);
+
+        assert_eq!(true, true);
+    }
+
+    // init tests
+    #[test]
+    fn game_input_init_no_bits_sets_proper_values() {
+        let max_bytes = 3;
+        let max_players = 2;
+        let byte_size_of_all_input_for_all_players = 3;
+
+        let mut input = GameInput::new(
+            max_bytes,
+            max_players,
+            byte_size_of_all_input_for_all_players,
+        );
+
+        let frame = 2;
+        let bits = None;
+        let byte_size_of_all_input_for_all_players = 4;
+
+        input.init(frame, bits, byte_size_of_all_input_for_all_players);
+
+        assert_eq!(frame, input.frame);
+        assert_eq!(
+            byte_size_of_all_input_for_all_players,
+            input.byte_size_of_all_input_for_all_players
+        );
+        assert_eq!(get_empty_bits(input.max_bytes), input.bits);
+    }
+
+    #[test]
+    fn game_input_init_has_bits_sets_proper_values() {
+        let max_bytes = 3;
+        let max_players = 2;
+        let byte_size_of_all_input_for_all_players = 3;
+
+        let mut input = GameInput::new(
+            max_bytes,
+            max_players,
+            byte_size_of_all_input_for_all_players,
+        );
+
+        let frame = 2;
+        let bits = vec![true, false, true, false];
+
+        let mut expected_bits = get_empty_bits(max_bytes);
+
+        for (i, bit) in bits.iter().enumerate() {
+            expected_bits[i] = *bit;
+        }
+
+        let byte_size_of_all_input_for_all_players = 4;
+        input.init(frame, Some(bits), byte_size_of_all_input_for_all_players);
+
+        assert_eq!(frame, input.frame);
+        assert_eq!(
+            byte_size_of_all_input_for_all_players,
+            input.byte_size_of_all_input_for_all_players
+        );
+        assert_eq!(expected_bits, input.bits);
+    }
+
+    #[test]
+    #[should_panic]
+    fn game_input_init_byte_size_all_inputs_greater_than_max_bytes_x_max_players_panics() {
+        let max_bytes = 3;
+        let max_players = 2;
+        let byte_size_of_all_input_for_all_players = 3;
+
+        let mut input = GameInput::new(
+            max_bytes,
+            max_players,
+            byte_size_of_all_input_for_all_players,
+        );
+
+        let frame = 2;
+        let bits = vec![true, false, true, false];
+
+        let mut expected_bits = get_empty_bits(max_bytes);
+
+        for (i, bit) in bits.iter().enumerate() {
+            expected_bits[i] = *bit;
+        }
+
+        let byte_size_of_all_input_for_all_players = 999999;
+        input.init(frame, Some(bits), byte_size_of_all_input_for_all_players);
+        assert_eq!(true, true);
+    }
+
+    // init_offset tests
+    #[test]
+    fn game_input_init_offset_no_bits_sets_proper_values() {
+        let max_bytes = 3;
+        let max_players = 2;
+        let byte_size_of_all_input_for_all_players = 2;
+
+        let mut input = GameInput::new(
+            max_bytes,
+            max_players,
+            byte_size_of_all_input_for_all_players,
+        );
+
+        let offset = 0;
+        let frame = 2;
+        let bits = None;
+        let byte_size_of_all_input_for_all_players = 3;
+
+        input.init_offset(frame, bits, byte_size_of_all_input_for_all_players, offset);
+
+        assert_eq!(frame, input.frame);
+        assert_eq!(
+            byte_size_of_all_input_for_all_players,
+            input.byte_size_of_all_input_for_all_players
+        );
+        assert_eq!(get_empty_bits(input.max_bytes), input.bits);
+    }
+
+    #[test]
+    fn game_input_init_offset_has_bits_sets_proper_values() {
+        let max_bytes = 3;
+        let max_players = 2;
+        let byte_size_of_all_input_for_all_players = 2;
+
+        let mut input = GameInput::new(
+            max_bytes,
+            max_players,
+            byte_size_of_all_input_for_all_players,
+        );
+
+        let offset = 0;
+        let frame = 2;
+        let bits = vec![true, false, true, false];
+
+        let mut expected_bits = get_empty_bits(max_bytes);
+
+        for (i, bit) in bits.iter().enumerate() {
+            expected_bits[i] = *bit;
+        }
+
+        let byte_size_of_all_input_for_all_players = 3;
+        input.init_offset(
+            frame,
+            Some(bits),
+            byte_size_of_all_input_for_all_players,
+            offset,
+        );
+
+        assert_eq!(frame, input.frame);
+        assert_eq!(
+            byte_size_of_all_input_for_all_players,
+            input.byte_size_of_all_input_for_all_players
+        );
+        assert_eq!(expected_bits, input.bits);
+    }
+
+    #[test]
+    fn game_input_init_offset_with_offset_no_bits_sets_proper_values() {
+        let max_bytes = 3;
+        let max_players = 2;
+        let byte_size_of_all_input_for_all_players = 2;
+
+        let mut input = GameInput::new(
+            max_bytes,
+            max_players,
+            byte_size_of_all_input_for_all_players,
+        );
+
+        let offset = 2;
+        let frame = 2;
+        let bits = None;
+        let byte_size_of_all_input_for_all_players = 3;
+
+        input.init_offset(frame, bits, byte_size_of_all_input_for_all_players, offset);
+
+        assert_eq!(frame, input.frame);
+        assert_eq!(
+            byte_size_of_all_input_for_all_players,
+            input.byte_size_of_all_input_for_all_players
+        );
+        assert_eq!(get_empty_bits(input.max_bytes), input.bits);
+    }
+
+    #[test]
+    fn game_input_init_offset_has_bits_with_offset3_sets_proper_values() {
+        let max_bytes = 5;
+        let max_players = 2;
+        let byte_size_of_all_input_for_all_players = 5;
+
+        let mut input = GameInput::new(
+            max_bytes,
+            max_players,
+            byte_size_of_all_input_for_all_players,
+        );
+
+        let offset = 3;
+        let frame = 2;
+        let bits = vec![true, false, true, false];
+
+        let mut expected_bits = get_empty_bits(max_bytes);
+
+        for (i, bit) in bits.iter().enumerate() {
+            expected_bits[i + (offset * byte_size_of_all_input_for_all_players)] = *bit;
+        }
+
+        let byte_size_of_all_input_for_all_players = 5;
+        input.init_offset(
+            frame,
+            Some(bits),
+            byte_size_of_all_input_for_all_players,
+            offset,
+        );
+
+        assert_eq!(frame, input.frame);
+        assert_eq!(
+            byte_size_of_all_input_for_all_players,
+            input.byte_size_of_all_input_for_all_players
+        );
+        assert_eq!(expected_bits, input.bits);
+    }
+
+    #[test]
+    fn game_input_init_offset_has_bits_with_offset1_sets_proper_values() {
+        let max_bytes = 5;
+        let max_players = 2;
+        let byte_size_of_all_input_for_all_players = 5;
+
+        let mut input = GameInput::new(
+            max_bytes,
+            max_players,
+            byte_size_of_all_input_for_all_players,
+        );
+
+        let offset = 1;
+        let frame = 2;
+        let bits = vec![true, false, true, false];
+
+        let mut expected_bits = get_empty_bits(max_bytes);
+
+        for (i, bit) in bits.iter().enumerate() {
+            expected_bits[i + (offset * byte_size_of_all_input_for_all_players)] = *bit;
+        }
+
+        let byte_size_of_all_input_for_all_players = 5;
+        input.init_offset(
+            frame,
+            Some(bits),
+            byte_size_of_all_input_for_all_players,
+            offset,
+        );
+
+        assert_eq!(frame, input.frame);
+        assert_eq!(
+            byte_size_of_all_input_for_all_players,
+            input.byte_size_of_all_input_for_all_players
+        );
+        assert_eq!(expected_bits, input.bits);
+    }
+
+    #[test]
+    #[should_panic]
+    fn game_input_init_offset_byte_size_all_inputs_greater_than_max_bytes_x_max_players_panics() {
+        let max_bytes = 3;
+        let max_players = 2;
+        let byte_size_of_all_input_for_all_players = 3;
+
+        let mut input = GameInput::new(
+            max_bytes,
+            max_players,
+            byte_size_of_all_input_for_all_players,
+        );
+
+        let frame = 2;
+        let bits = vec![true, false, true, false];
+        let offset = 0;
+
+        let mut expected_bits = get_empty_bits(max_bytes);
+
+        for (i, bit) in bits.iter().enumerate() {
+            expected_bits[i] = *bit;
+        }
+
+        let byte_size_of_all_input_for_all_players = 999999;
+        input.init_offset(
+            frame,
+            Some(bits),
+            byte_size_of_all_input_for_all_players,
+            offset,
+        );
+        assert_eq!(true, true);
+    }
+
+    // is_null tests
+    #[test]
+    fn game_input_is_null_not_null_frame_returns_false() {
+        let max_bytes = 5;
+        let max_players = 2;
+        let byte_size_of_all_input_for_all_players = 5;
+
+        let mut input = GameInput::new(
+            max_bytes,
+            max_players,
+            byte_size_of_all_input_for_all_players,
+        );
+
+        let offset = 1;
+        let frame = 2;
+        let bits = vec![true, false, true, false];
+
+        let mut expected_bits = get_empty_bits(max_bytes);
+
+        for (i, bit) in bits.iter().enumerate() {
+            expected_bits[i + (offset * byte_size_of_all_input_for_all_players)] = *bit;
+        }
+
+        let byte_size_of_all_input_for_all_players = 5;
+        input.init_offset(
+            frame,
+            Some(bits),
+            byte_size_of_all_input_for_all_players,
+            offset,
+        );
+
+        let actual = input.is_null();
+        let expected = false;
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn game_input_is_null_null_frame_returns_true() {
+        let max_bytes = 5;
+        let max_players = 2;
+        let byte_size_of_all_input_for_all_players = 5;
+
+        let mut input = GameInput::new(
+            max_bytes,
+            max_players,
+            byte_size_of_all_input_for_all_players,
+        );
+
+        let offset = 1;
+        let frame = NULL_FRAME;
+        let bits = vec![true, false, true, false];
+
+        let mut expected_bits = get_empty_bits(max_bytes);
+
+        for (i, bit) in bits.iter().enumerate() {
+            expected_bits[i + (offset * byte_size_of_all_input_for_all_players)] = *bit;
+        }
+
+        let byte_size_of_all_input_for_all_players = 5;
+        input.init_offset(
+            frame,
+            Some(bits),
+            byte_size_of_all_input_for_all_players,
+            offset,
+        );
+
+        let actual = input.is_null();
+        let expected = true;
+
+        assert_eq!(expected, actual);
+    }
+
+    // value tests
+    #[test]
+    fn game_input_value_index_2_returns_false() {
+        let max_bytes = 5;
+        let max_players = 2;
+        let byte_size_of_all_input_for_all_players = 5;
+
+        let mut input = GameInput::new(
+            max_bytes,
+            max_players,
+            byte_size_of_all_input_for_all_players,
+        );
+
+        let frame = NULL_FRAME;
+        let bits = vec![true, false, false, false];
+
+        let mut expected_bits = get_empty_bits(max_bytes);
+
+        for (i, bit) in bits.iter().enumerate() {
+            expected_bits[i] = *bit;
+        }
+
+        let byte_size_of_all_input_for_all_players = 5;
+        input.init(frame, Some(bits), byte_size_of_all_input_for_all_players);
+
+        let actual = input.value(2);
+        let expected = false;
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn game_input_value_index_4_returns_true() {
+        let max_bytes = 5;
+        let max_players = 2;
+        let byte_size_of_all_input_for_all_players = 5;
+
+        let mut input = GameInput::new(
+            max_bytes,
+            max_players,
+            byte_size_of_all_input_for_all_players,
+        );
+
+        let frame = NULL_FRAME;
+        let bits = vec![true, false, false, false, true];
+
+        let mut expected_bits = get_empty_bits(max_bytes);
+
+        for (i, bit) in bits.iter().enumerate() {
+            expected_bits[i] = *bit;
+        }
+
+        let byte_size_of_all_input_for_all_players = 5;
+        input.init(frame, Some(bits), byte_size_of_all_input_for_all_players);
+
+        let actual = input.value(4);
+        let expected = true;
+
+        assert_eq!(expected, actual);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ mod ringbuffer;
 mod rmercury;
 mod rplayer;
 mod rsystem;
+mod utility;
 
 pub use self::rmercury::{
     rmercury_start_session, rmercury_start_spectator, rmercury_start_synctest, RSession,
